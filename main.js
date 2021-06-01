fe7e101bfd6685b6750ed7ea4e03a57442bcf5ab
GrabovyiS <ecclesiasth@gmail.com>
2021-06-01 13:36:28 +0300

    Верстка

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+function createnew(){
+    let new_li=document.createElement('li');
+    let new_h3=document.createElement('h3');
+    let new_p=document.createElement('p');
+    let new_categ=document.createElement('p');
+
+    new_li.classList.add('list_li');
+    new_h3.classList.add('list_h3');
+    new_p.classList.add('list_text');
+    new_categ.classList.add('list_text');
+}
